05346952d013ef04bc2239f9f8550ae6e0d0a921
Croxx <mrcroxx@outlook.com>
2023-05-19 14:39:56 +0800

    feat: introduce generic policy (#6)
    Signed-off-by: MrCroxx <mrcroxx@outlook.com>

--- src/lib.rs
+++ src/lib.rs
@@ -12,5 +12,12 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 
+use policies::Policy;
+
 pub mod collections;
 pub mod policies;
+
+#[allow(unused)]
+pub struct Cache<P: Policy> {
+    policy: P,
+}
