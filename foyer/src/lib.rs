1ea57ad8377ec49fdcd58740e21daa9a1896a6c1
Croxx <mrcroxx@outlook.com>
2023-07-03 16:18:52 +0800

    Revert "fix: export extern crate (#38)" (#39)
    This reverts commit a8a56ccc19756d4f4b7ac253971163b4dcfda637.

--- foyer/src/lib.rs
+++ foyer/src/lib.rs
@@ -27,6 +27,6 @@ impl Weight for Vec<u8> {
     }
 }
 
-pub extern crate foyer_common as common;
-pub extern crate foyer_intrusive as intrusive;
-pub extern crate foyer_storage as storage;
+pub use foyer_common as common;
+pub use foyer_intrusive as intrusive;
+pub use foyer_storage as storage;
