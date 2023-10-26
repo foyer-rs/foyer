1a58f53ac1af69daec77a3131c7e92df7c1983c1
MrCroxx <mrcroxx@outlook.com>
2023-10-26 17:54:07 +0800

    chore: only keep minized dashboard
    Signed-off-by: MrCroxx <mrcroxx@outlook.com>

--- scripts/minimize-dashboards.sh
+++ scripts/minimize-dashboards.sh
@@ -7,13 +7,17 @@ set -e
 
 DIR="etc/grafana/dashboards"
 
-
 for dashboard in "${DIR}"/*.json; do
     if [[ "$dashboard" == *.min.json ]]; then
         continue
     fi
     name=$(basename "$dashboard" .json)
     jq -c < "$dashboard" > "${DIR}/${name}.min.json"
+done
+
+for dashboard in "${DIR}"/*.min.json; do
+    name=$(basename "$dashboard" .min.json)
+    mv "$dashboard" "${DIR}/${name}.json"
 done
 
 if [ "$1" == "--check" ] ; then
