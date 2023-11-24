dd8ad1f56523d077571d1794da2284364486b1be
MrCroxx <mrcroxx@outlook.com>
2023-11-24 12:39:44 +0800

    chore: upgrade hyper to 1.0
    Signed-off-by: MrCroxx <mrcroxx@outlook.com>

--- foyer-storage-bench/src/export.rs
+++ foyer-storage-bench/src/export.rs
@@ -15,12 +15,16 @@
 use std::net::SocketAddr;
 
 use foyer_storage::metrics::get_metrics_registry;
+use http_body_util::Full;
 use hyper::{
+    body::{Body, Bytes},
     header::CONTENT_TYPE,
-    service::{make_service_fn, service_fn},
-    Body, Error, Request, Response, Server,
+    service::service_fn,
+    Request, Response,
 };
+
 use prometheus::{Encoder, TextEncoder};
+use tokio::net::TcpListener;
 
 pub struct MetricsExporter;
 
@@ -28,18 +32,32 @@ impl MetricsExporter {
     pub fn init(addr: SocketAddr) {
         tokio::spawn(async move {
             tracing::info!("Prometheus service is set up on http://{}", addr);
-            if let Err(e) = Server::bind(&addr)
-                .serve(make_service_fn(|_| async move {
-                    Ok::<_, Error>(service_fn(Self::serve))
-                }))
-                .await
-            {
-                tracing::error!("Prometheus service error: {}", e);
+
+            let listener = TcpListener::bind(addr).await.unwrap();
+            loop {
+                let stream = match listener.accept().await {
+                    Ok((stream, _addr)) => stream,
+                    Err(e) => {
+                        tracing::error!("accept conn error: {}", e);
+                        continue;
+                    }
+                };
+                let io = hyper_util::rt::TokioIo::new(stream);
+                tokio::spawn(async move {
+                    if let Err(e) = hyper_util::server::conn::auto::Builder::new(
+                        hyper_util::rt::TokioExecutor::new(),
+                    )
+                    .serve_connection(io, service_fn(Self::serve))
+                    .await
+                    {
+                        tracing::error!("Prometheus service error: {}", e);
+                    }
+                });
             }
         });
     }
 
-    async fn serve(_request: Request<Body>) -> anyhow::Result<Response<Body>> {
+    async fn serve(_request: Request<impl Body + Sized>) -> anyhow::Result<Response<Full<Bytes>>> {
         let encoder = TextEncoder::new();
         let mut buffer = Vec::with_capacity(4096);
         let metrics = get_metrics_registry().gather();
@@ -47,7 +65,7 @@ impl MetricsExporter {
         let response = Response::builder()
             .status(200)
             .header(CONTENT_TYPE, encoder.format_type())
-            .body(Body::from(buffer))?;
+            .body(Full::new(Bytes::from(buffer)))?;
         Ok(response)
     }
 }
