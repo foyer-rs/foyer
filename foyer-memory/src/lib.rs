50e5dee70acff6739ed176db690c4e2cbe832fbc
MrCroxx <mrcroxx@outlook.com>
2024-02-22 11:37:50 +0800

    feat: introduce foyer-memory Cache abstraction
    Signed-off-by: MrCroxx <mrcroxx@outlook.com>

--- foyer-memory/src/lib.rs
+++ foyer-memory/src/lib.rs
@@ -12,6 +12,8 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 
+pub mod v2;
+
 use std::{hash::Hasher, sync::Arc};
 
 use foyer_common::code::{Key, Value};
