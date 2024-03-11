e1ed5c034a0b49b6e7f610800f90170cb29e6666
Croxx <mrcroxx@outlook.com>
2024-03-11 11:55:02 +0800

    refactor: use generic type instead of associated type for listener
    * refactor: make context listener's generic type and alias for simplicity
    
    Signed-off-by: MrCroxx <mrcroxx@outlook.com>
    
    * refactor: remove context from listener, because cannot impl trait alias
    
    Signed-off-by: MrCroxx <mrcroxx@outlook.com>
    
    * refactor: restore context for listener, make user suffer
    
    Signed-off-by: MrCroxx <mrcroxx@outlook.com>
    
    ---------
    
    Signed-off-by: MrCroxx <mrcroxx@outlook.com>

--- foyer-memory/src/listener.rs
+++ foyer-memory/src/listener.rs
@@ -14,45 +14,42 @@
 
 use std::marker::PhantomData;
 
-use crate::{Key, Value};
+use crate::{Context, Key, Value};
 
-pub trait CacheEventListener: Send + Sync + 'static {
-    type Key: Key;
-    type Value: Value;
-    type Context: Send + Sync + 'static;
-
+pub trait CacheEventListener<K, V, C>: Send + Sync + 'static
+where
+    K: Key,
+    V: Value,
+    C: Context,
+{
     /// The function is called when an entry is released by the cache and all external users.
     ///
-    /// The arguments includes the key, value and context with ownership.
-    fn on_release(&self, key: Self::Key, value: Self::Value, context: Self::Context, charges: usize);
+    /// The arguments includes the key and value with ownership.
+    fn on_release(&self, key: K, value: V, context: C, charges: usize);
 }
 
 pub struct DefaultCacheEventListener<K, V, C>(PhantomData<(K, V, C)>)
 where
     K: Key,
     V: Value,
-    C: Send + Sync + 'static;
+    C: Context;
 
 impl<K, V, C> Default for DefaultCacheEventListener<K, V, C>
 where
     K: Key,
     V: Value,
-    C: Send + Sync + 'static,
+    C: Context,
 {
     fn default() -> Self {
         Self(Default::default())
     }
 }
 
-impl<K, V, C> CacheEventListener for DefaultCacheEventListener<K, V, C>
+impl<K, V, C> CacheEventListener<K, V, C> for DefaultCacheEventListener<K, V, C>
 where
     K: Key,
     V: Value,
-    C: Send + Sync + 'static,
+    C: Context,
 {
-    type Key = K;
-    type Value = V;
-    type Context = C;
-
-    fn on_release(&self, _key: Self::Key, _value: Self::Value, _context: Self::Context, _charges: usize) {}
+    fn on_release(&self, _key: K, _value: V, _context: C, _charges: usize) {}
 }
