a25ee768b4545ba69494b91dd23cf465458a0f96
MrCroxx <mrcroxx@outlook.com>
2024-03-12 11:17:27 +0800

    refactor: add eq for cache context
    Signed-off-by: MrCroxx <mrcroxx@outlook.com>

--- foyer-memory/src/context.rs
+++ foyer-memory/src/context.rs
@@ -12,7 +12,7 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum CacheContext {
     /// The default context shared by all eviction container implementations.
     Default,
