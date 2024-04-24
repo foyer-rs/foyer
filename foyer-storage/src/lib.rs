381752e6f0c202cd0c9c5a1bb3839122e2fbc2ae
MrCroxx <mrcroxx@outlook.com>
2024-04-24 17:35:45 +0800

    fix: fix build with night feature
    Signed-off-by: MrCroxx <mrcroxx@outlook.com>

--- foyer-storage/src/lib.rs
+++ foyer-storage/src/lib.rs
@@ -12,6 +12,8 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 
+#![cfg_attr(feature = "nightly", feature(allocator_api))]
+
 mod admission;
 mod buffer;
 mod catalog;
--- foyer/src/lib.rs
+++ foyer/src/lib.rs
@@ -12,6 +12,8 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 
+#![cfg_attr(feature = "nightly", feature(allocator_api))]
+
 use foyer_memory as memory;
 use foyer_storage as storage;
 
