afb54929ad1c6251a5f91a2a4c01a5e39d0fbd3f
MrCroxx <mrcroxx@outlook.com>
2024-05-23 12:06:01 +0800

    chore: use cargo install to install deps instead of cargo-binstall
    Signed-off-by: MrCroxx <mrcroxx@outlook.com>

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-
-if [ -z "$(which cargo-binstall)" ]; then
-    curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
-fi
-
-cargo binstall -y cargo-sort cargo-nextest typos-cli
+cargo install cargo-sort cargo-nextest typos-cli cargo-udeps
