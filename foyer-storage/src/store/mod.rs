a8cc1222cb8aff7fcd987b2f762231dd7be44405
Li0k <yuli@singularity-data.com>
2024-06-21 17:01:06 +0800

    fix(foyer): typo

--- foyer-storage/src/store/mod.rs
+++ foyer-storage/src/store/mod.rs
@@ -314,7 +314,7 @@ where
     reclaimers: usize,
     clean_region_threshold: Option<usize>,
     eviction_pickers: Vec<Box<dyn EvictionPicker>>,
-    admision_picker: Arc<dyn AdmissionPicker<Key = K>>,
+    admission_picker: Arc<dyn AdmissionPicker<Key = K>>,
     reinsertion_picker: Arc<dyn ReinsertionPicker<Key = K>>,
     compression: Compression,
     tombstone_log_config: Option<TombstoneLogConfig>,
@@ -342,7 +342,7 @@ where
             reclaimers: 1,
             clean_region_threshold: None,
             eviction_pickers: vec![Box::new(InvalidRatioPicker::new(0.8)), Box::<FifoPicker>::default()],
-            admision_picker: Arc::<AdmitAllPicker<K>>::default(),
+            admission_picker: Arc::<AdmitAllPicker<K>>::default(),
             reinsertion_picker: Arc::<RejectAllPicker<K>>::default(),
             compression: Compression::None,
             tombstone_log_config: None,
@@ -451,7 +451,7 @@ where
     ///
     /// Default: [`AdmitAllPicker`].
     pub fn with_admission_picker(mut self, admission_picker: Arc<dyn AdmissionPicker<Key = K>>) -> Self {
-        self.admision_picker = admission_picker;
+        self.admission_picker = admission_picker;
         self
     }
 
@@ -519,7 +519,7 @@ where
                     reclaimers: self.reclaimers,
                     clean_region_threshold,
                     eviction_pickers: self.eviction_pickers,
-                    admission_picker: self.admision_picker,
+                    admission_picker: self.admission_picker,
                     reinsertion_picker: self.reinsertion_picker,
                     tombstone_log_config: self.tombstone_log_config,
                 }))
@@ -540,7 +540,7 @@ where
                         reclaimers: self.reclaimers,
                         clean_region_threshold,
                         eviction_pickers: self.eviction_pickers,
-                        admission_picker: self.admision_picker,
+                        admission_picker: self.admission_picker,
                         reinsertion_picker: self.reinsertion_picker,
                         tombstone_log_config: self.tombstone_log_config,
                     },
