e6a7fdaaecd366d9a6133d3ca5c33dab8aec69ca
Croxx <mrcroxx@outlook.com>
2024-07-15 15:29:46 +0800

    feat: introduce compact bloom filter (#612)
    * feat: introduce compact bloom filter
    
    Signed-off-by: MrCroxx <mrcroxx@outlook.com>
    
    * chore: impl debug for cbf shard
    
    Signed-off-by: MrCroxx <mrcroxx@outlook.com>
    
    ---------
    
    Signed-off-by: MrCroxx <mrcroxx@outlook.com>

--- foyer-common/src/lib.rs
+++ foyer-common/src/lib.rs
@@ -27,6 +27,8 @@ pub mod bits;
 pub mod buf;
 /// The trait for the key and value encoding and decoding.
 pub mod code;
+/// Bloom filters.
+pub mod compact_bloom_filter;
 /// A concurrent count down util.
 pub mod countdown;
 /// Components for monitoring internal events.
