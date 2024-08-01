d10ef6e963f635f5a5d4f4b544620846132d0064
MrCroxx <mrcroxx@outlook.com>
2024-08-01 18:35:10 +0800

    refactor: fix warnings for toolchain 1.80
    Signed-off-by: MrCroxx <mrcroxx@outlook.com>

--- foyer-common/src/lib.rs
+++ foyer-common/src/lib.rs
@@ -12,7 +12,6 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 
-#![cfg_attr(coverage_nightly, feature(coverage_attribute))]
 #![warn(missing_docs)]
 
 //! Shared components and utils for foyer.
