f90095565380e88bc466ff50b2448d065d68d325
MrCroxx <mrcroxx@outlook.com>
2024-08-29 03:58:21 +0000

    chore: fix typo
    Signed-off-by: MrCroxx <mrcroxx@outlook.com>

--- foyer/src/hybrid/cache.rs
+++ foyer/src/hybrid/cache.rs
@@ -257,7 +257,7 @@ where
 
     /// Get cached entry with the given key from the hybrid cache.
     ///
-    /// Different from `get`, `obtain` dedeplicates the disk cache queries.
+    /// Different from `get`, `obtain` deduplicates the disk cache queries.
     ///
     /// `obtain` is always supposed to be used instead of `get` if the overhead of getting the ownership of the given
     /// key is acceptable.
