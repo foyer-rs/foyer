109845b47e003132e310b58b1f9f03fc4e18ef58
MrCroxx <mrcroxx@outlook.com>
2024-09-09 04:52:32 +0000

    feat: introduce foyer-cli framework
    Signed-off-by: MrCroxx <mrcroxx@outlook.com>

---
+++ foyer-cli/src/main.rs
@@ -0,0 +1,41 @@
+//  Copyright 2024 Foyer Project Authors
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+//! `foyer-cli` provides debug tools for foyer,
+
+mod args;
+
+use args::ArgsArgs;
+use clap::{Parser, Subcommand};
+
+#[derive(Debug, Parser)]
+#[command(author, version, about)]
+pub struct Cli {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Debug, Subcommand)]
+pub enum Command {
+    /// Automatic arguments detector.
+    Args(ArgsArgs),
+}
+
+fn main() {
+    let cli = Cli::parse();
+
+    match cli.command {
+        Command::Args(args) => args::run(args),
+    }
+}
