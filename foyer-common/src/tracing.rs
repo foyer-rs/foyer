f5d0cf46e6612695c244fe4c75eae56dcf5481f5
yihong0618 <zouzou0208@gmail.com>
2025-03-18 16:10:21 +0800

    fix(ci): make clippy happy
    Signed-off-by: yihong0618 <zouzou0208@gmail.com>

--- foyer-common/src/tracing.rs
+++ foyer-common/src/tracing.rs
@@ -206,7 +206,7 @@ where
             Poll::Pending => return Poll::Pending,
         };
 
-        let mut root = this.root.take().unwrap();
+        let root = this.root.take().unwrap();
 
         if let (Some(elapsed), Some(threshold)) = (root.elapsed(), this.threshold.as_ref()) {
             if &elapsed < threshold {
