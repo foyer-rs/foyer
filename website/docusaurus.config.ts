eba639a4feae73e8837d5d923a44c66459d27b9c
MrCroxx <mrcroxx@outlook.com>
2025-09-15 22:06:48 +0800

    fix: fix website base url
    Signed-off-by: MrCroxx <mrcroxx@outlook.com>

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -8,10 +8,10 @@ const config: Config = {
   favicon: 'img/favicon.svg',
 
   // Set the production url of your site here
-  url: 'https://foyer.rs',
+  url: 'https://foyer-rs.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/foyer/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
